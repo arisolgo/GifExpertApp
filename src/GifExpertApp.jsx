a492c0ea30636f3867c59d8cc88d3662e30784e7
Ariel Virgilio Solano González <arisolgo@MacBook-Pro-de-Ariel.local>
2022-08-30 20:38:04 -0400

    creating the capitalize function

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { capitalize } from "./utils/commonFunctions";
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
   const addCategory = (category) => {
-    setCategories([...categories, category]);
+    const newCategory = capitalize(category);
+    if (categories.includes(newCategory)) return;
+    setCategories([...categories, newCategory]);
   };
   return (
     <>
       <h1>GifExpertApp</h1>
-      <AddCategory
-        onNewCategory={addCategory}
-        // addCategory={setCategories}
-      />
+      <AddCategory onNewCategory={addCategory} />
       <button onClick={addCategory}>Add Category</button>
       <ol>
         {categories.map((category) => {
---
+++ src/utils/commonFunctions.js
@@ -0,0 +1,3 @@
+export const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.toLowerCase().slice(1);
+};
